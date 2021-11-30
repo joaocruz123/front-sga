7e0b52d7ec04b4de62a574d5fbb57cf2969c1d92
João Cruz <joao.cruz@redspark.io>
2021-11-30 02:47:13 -0300

    Ajuste para mensagem de tabela vazia

--- src/pages/membros/Membros.vue
+++ src/pages/membros/Membros.vue
@@ -10,71 +10,83 @@
           <q-icon name="assignment_ind" class="q-mb-sm" /> Membros
         </div>
 
-        <q-table
-          :data="membros"
-          :columns="columns"
-          row-key="name"
-          bordered
-          color="primary"
-          card-class="bg-grey-11 text-black"
-        >
-          <!-- <template v-slot:top>
-                    <div class="col" />
-                    <q-btn color="secondary" icon="add" label="new" @click="$router.push({ name: 'membro', params: { id: 0 } })" />
-                </template> -->
+        <div v-if="membros && !membros.length > 0">
+          <q-banner inline-actions rounded class="bg-primary text-white">
+            Não há membros cadastrados para serem exibidos.
 
-          <template v-slot:header="props">
-            <q-tr :props="props">
-              <q-th v-for="col in props.cols" :key="col.name" :props="props">
-                {{ col.label }}
-              </q-th>
-              <q-th auto-width />
-            </q-tr>
-          </template>
+            <!--<template v-slot:action>
+              <q-btn flat label="Cadastrar" />
+            </template>-->
+          </q-banner>
+        </div>
+        <div v-else>
+          <q-table
+            :data="membros"
+            :columns="columns"
+            row-key="name"
+            bordered
+            color="primary"
+            card-class="bg-grey-11 text-black"
+          >
+            <template v-slot:header="props">
+              <q-tr :props="props">
+                <q-th v-for="col in props.cols" :key="col.name" :props="props">
+                  {{ col.label }}
+                </q-th>
+                <q-th auto-width />
+              </q-tr>
+            </template>
 
-          <template v-slot:body="props">
-            <q-tr :props="props">
-              <q-td v-for="col in props.cols" :key="col.name" :props="props">
-                <div v-if="col.name === 'avatar'">
-                  <q-avatar>
-                    <img :src="`http://localhost/uploads/avatars/${col.value}`" />
-                  </q-avatar>
-                </div>
-                <div v-if="col.name === 'afastado'">
-                  <div v-if="col.value === 'não'">
-                    <q-badge color="red"> Não </q-badge>
+            <template v-slot:body="props">
+              <q-tr :props="props">
+                <q-td v-for="col in props.cols" :key="col.name" :props="props">
+                  <div v-if="col.name === 'avatar'">
+                    <q-avatar>
+                      <img
+                        :src="`http://localhost/uploads/avatars/${col.value}`"
+                      />
+                    </q-avatar>
                   </div>
-                  <div v-if="col.value === 'null'">
-                    <q-badge color="red"> Não </q-badge>
+                  <div v-if="col.name === 'afastado'">
+                    <div v-if="col.value === 'não'">
+                      <q-badge color="red"> Não </q-badge>
+                    </div>
+                    <div v-if="col.value === 'null'">
+                      <q-badge color="red"> Não </q-badge>
+                    </div>
+                    <div v-if="col.value === 'sim'">
+                      <q-badge color="green"> Sim </q-badge>
+                    </div>
                   </div>
-                  <div v-if="col.value === 'sim'">
-                    <q-badge color="green"> Sim </q-badge>
+                  <div v-if="col.name !== 'avatar' && col.name !== 'afastado'">
+                    {{ col.value }}
                   </div>
-                </div>
-                <div v-if="col.name !== 'avatar' && col.name !== 'afastado'">
-                  {{ col.value }}
-                </div>
-              </q-td>
-              <q-td auto-width>
-                <q-btn
-                  flat
-                  round
-                  color="secondary"
-                  icon="edit"
-                  @click="$router.push({ name: 'membro', params: { id: props.row.id } })"
-                />
-                <q-btn
-                  flat
-                  round
-                  color="negative"
-                  icon="delete_outline"
-                  @click="confirmRemove(props.row.id)"
-                />
-              </q-td>
-            </q-tr>
-          </template>
-        </q-table>
-
+                </q-td>
+                <q-td auto-width>
+                  <q-btn
+                    flat
+                    round
+                    color="secondary"
+                    icon="edit"
+                    @click="
+                      $router.push({
+                        name: 'membro',
+                        params: { id: props.row.id },
+                      })
+                    "
+                  />
+                  <q-btn
+                    flat
+                    round
+                    color="negative"
+                    icon="delete_outline"
+                    @click="confirmRemove(props.row.id)"
+                  />
+                </q-td>
+              </q-tr>
+            </template>
+          </q-table>
+        </div>
         <q-dialog
           v-model="confirm_remove"
           persistent
@@ -85,16 +97,24 @@
             <q-bar>
               <q-space />
               <q-btn dense flat icon="close" v-close-popup>
-                <q-tooltip content-class="bg-white text-primary">Close</q-tooltip>
+                <q-tooltip content-class="bg-white text-primary"
+                  >Close</q-tooltip
+                >
               </q-btn>
             </q-bar>
 
             <q-card-section class="row items-center">
-              <span class="q-ml-sm">Deseja Remover o item PERMANENTEMENTE?</span>
+              <span class="q-ml-sm"
+                >Deseja Remover o item PERMANENTEMENTE?</span
+              >
             </q-card-section>
 
             <q-card-actions align="right">
-              <q-btn label="Cancel" color="accent" @click="confirm_remove = false" />
+              <q-btn
+                label="Cancel"
+                color="accent"
+                @click="confirm_remove = false"
+              />
               <q-btn label="Remover" color="negative" @click="removeItem()" />
             </q-card-actions>
           </q-card>
@@ -141,7 +161,12 @@
         </q-card-section>
 
         <q-card-actions align="right">
-          <q-btn label="Cancel" outline color="grey-7" @click="confirm_remove = false" />
+          <q-btn
+            label="Cancel"
+            outline
+            color="grey-7"
+            @click="confirm_remove = false"
+          />
           <q-btn label="Remover" color="negative" @click="removeItem()" />
         </q-card-actions>
       </q-card>
